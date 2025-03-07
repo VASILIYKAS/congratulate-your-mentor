17e58fbb7e424878b0caf3741f4c462c578a330c
VASILIYKAS <yoyoynv@gmail.com>
2025-03-07 18:03:35 +0500

    содержимое файла validators.py перенесено в api_client

--- libs/api_client.py
+++ libs/api_client.py
@@ -1,31 +1,42 @@
 import httpx
-from validators import MentorsResponse, Congratulations, AsciiArt
-from pydantic import ValidationError
+from pydantic import BaseModel
+from typing import List
+
+
+class Mentor(BaseModel):
+    last_name: str
+    first_name: str
+    tg_id: int
+    user_name: str
+
+
+class MentorsResponse(BaseModel):
+    mentors: List[Mentor]
+
+
+class Congratulations(BaseModel):
+    congratulations: List[str]
+
+
+class AsciiArt(BaseModel):
+    ASCIIART: List[str]
 
 
 def get_mentors_or_congratulations(url, endpoint):
-    try:
-        if endpoint == 'mentors':
-            response_model = MentorsResponse
-        elif endpoint == 'congratulations':
-            response_model = Congratulations
-        elif endpoint == 'asciiart':
-            response_model = AsciiArt
-        else:
-            raise ValueError("Неверный тип данных")
+    if endpoint == 'mentors':
+        response_model = MentorsResponse
+    elif endpoint == 'congratulations':
+        response_model = Congratulations
+    elif endpoint == 'asciiart':
+        response_model = AsciiArt
+    else:
+        raise ValueError("Неверный тип данных")
 
-        response = httpx.get(url)
-        response.status_code
-        response.raise_for_status()
-        external_data = response.json()
+    response = httpx.get(url)
+    response.status_code
+    response.raise_for_status()
+    external_data = response.json()
 
-        response_object = response_model(**external_data)
+    response_object = response_model(**external_data)
 
-        return response_object
-
-    except ValidationError as e:
-        raise e
-    except httpx.HTTPError as e:
-        raise e
-    except Exception as e:
-        raise e
+    return response_object
