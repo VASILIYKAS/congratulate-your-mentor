867943c0161541e3bb324967b6f02039abc6f845
VASILIYKAS <yoyoynv@gmail.com>
2025-03-11 19:46:34 +0500

    внесены изминения для работы с новой схемой api

--- libs/api_client.py
+++ libs/api_client.py
@@ -1,21 +1,29 @@
 import httpx
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
 
 
 class Mentor(BaseModel):
-    last_name: str
-    first_name: str
-    tg_id: int
-    user_name: str
+    id: int
+    name: dict
+    tg_username: str
+    tg_chat_id: int
+    bday: Optional[str] = None
 
 
 class MentorsResponse(BaseModel):
     mentors: List[Mentor]
 
 
-class Congratulations(BaseModel):
-    congratulations: List[str]
+class Postcard(BaseModel):
+    id: int
+    holidayId: str
+    name_ru: str
+    body: str
+
+
+class PostcardsResponse(BaseModel):
+    postcards: List[Postcard]    
 
 
 class AsciiArt(BaseModel):
@@ -23,17 +31,15 @@ class AsciiArt(BaseModel):
 
 
 def get_mentors_or_congratulations(url, endpoint):
-    if endpoint == 'mentors':
+    if endpoint == '/mentors':
         response_model = MentorsResponse
-    elif endpoint == 'congratulations':
-        response_model = Congratulations
-    elif endpoint == 'asciiart':
-        response_model = AsciiArt
+    elif endpoint == '/postcards':
+        response_model = PostcardsResponse
     else:
         raise ValueError("Неверный тип данных")
 
-    response = httpx.get(url)
-    response.status_code
+    full_url = f"{url}{endpoint}"
+    response = httpx.get(full_url)
     response.raise_for_status()
     external_data = response.json()
 
